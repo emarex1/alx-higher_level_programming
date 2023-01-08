e98f7ce8918ba2f4d74892dc5b52f7da766491e9
emarex1 <emmanuelawoh0@gmail.com>
2023-01-09 00:28:27 +0100

    fixed error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-add = __import__("add_0").add
-
-
-a = 1
-b = 2
-add(a, b)
-print("{} + {} = {}".format(a, b, a+b))
+if __name__ = "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
