1eb01f975e60c8f2dd23a428a19e550d4712f824
emarex1 <emmanuelawoh0@gmail.com>
2023-01-24 20:21:44 +0100

    safe division printing

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = a / b
+        return c
+    except ZeroDivisionError:
+        c = None
+        return
+    finally:
+        print("Inside result: {}".format(c))
