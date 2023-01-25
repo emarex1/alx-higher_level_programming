e0e21caaaa4b4f1860a395e0541dd52bc58138eb
emarex1 <emmanuelawoh0@gmail.com>
2023-01-25 07:54:12 +0100

    fixed an error

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    raise NameError
-    try:
-        raise_exception_msg(message="C is fun")
-    except NameError:
-        print(message)
+    raise NameError(message)
