4295d2ece59a19a16559492fcb9638576723491f
emarex1 <emmanuelawoh0@gmail.com>
2023-01-27 21:56:55 +0100

    safe print

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        import sys
+        print("Exception:", e, file=sys.stderr)
+        return None
