60ca1cf294f5c369038c35e22c4e21e74ed264ce
emarex1 <emmanuelawoh0@gmail.com>
2023-01-27 21:19:24 +0100

    safe division of a list by another list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    try:
-        list_length = [m/n for m, n in zip(my_list_1, my_list_2)]
-    except (TypeError, ValueError):
-        print("wrong type")
-    except ZeroDivisionError:
-        print("division by 0")
-    finally:
-        print("{}".format(list_length), end="")
-    if len(my_list_1) != len(my_list_2):
-        print("out of range")
+    new_list = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print('division by 0')
+            result = 0
+        except TypeError:
+            print('wrong type')
+            result = 0
+        except IndexError:
+            print('out of range')
+            result = 0
+        finally:
+            new_list.append(result)
+
+    return new_list
