474fedbc97fc6b474511402d0081ab1f6aa44ec2
emarex1 <emmanuelawoh0@gmail.com>
2023-02-06 17:05:05 +0100

    fixed error to pass checker

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,9 +3,8 @@
 
 
 def is_same_class(obj, a_class):
-    """ Check if isinstance. """
-
-    if isinstance(obj, a_class):
+    """ Check if obj is isinstance of a_class. """
+    if type(obj) == a_class:
         return True
     else:
         return False
