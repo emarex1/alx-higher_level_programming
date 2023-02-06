7bf7b9e13d1b6c7d794c43b460eb9a66cbdd39fe
emarex1 <emmanuelawoh0@gmail.com>
2023-02-06 17:00:25 +0100

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Define is_same_class. """
+
+
+def is_same_class(obj, a_class):
+    """ Check if isinstance. """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
