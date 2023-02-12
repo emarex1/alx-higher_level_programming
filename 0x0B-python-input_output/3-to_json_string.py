768ff9fa3a5c1458cd1f3e5d880e449f9676129d
emarex1 <emmanuelawoh0@gmail.com>
2023-02-12 16:45:41 +0100

    fixed an error to pass checker

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Define to_json_string. """
+""" Define to_json_string functions. """
 import json
 
 
 def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
     return json.dumps(my_obj)
