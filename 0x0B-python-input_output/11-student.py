c96e72e670ae40176b5a3586611323472283bee5
emarex1 <emmanuelawoh0@gmail.com>
2023-02-13 09:05:53 +0100

    task 11 python-input_output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Defines class Student. """
+
+
+class Student:
+    """ instantiates first_name, last_name and age"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    """ Returns to_json dictionary. """
+    def to_json(self, attr=None):
+        """ retrieves a dictionary representation of Student attr(list)"""
+        if attr is not None:
+            res = {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+            return res
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student."""
+        for k, v in json.items():
+            setattr(self, k, v)
