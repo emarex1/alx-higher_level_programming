d09bc07945baa93c0bf7cb2a9682343d7f315ee1
emarex1 <emmanuelawoh0@gmail.com>
2023-06-14 13:16:47 +0100

    error fix to pass checker

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-const size = Math.floor(Number(arg));
-
-if (Number.isNaN(size)) {
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  let i; let j = 0;
-  let square = '';
-  for (i = 0; i < size; i++) {
-    for (j = 0; j < size; j++) {
-      square += 'X';
-    } square += '\n';
-  } console.log(square);
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
+  }
 }
