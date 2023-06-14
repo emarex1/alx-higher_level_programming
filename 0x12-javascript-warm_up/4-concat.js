c75371e3feb82d4fb135133d795a2dd5e42e22ee
emarex1 <emmanuelawoh0@gmail.com>
2023-06-14 06:11:04 +0100

    error fix

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
-
-if (!arg1 || !arg2) {
-  console.log('Insufficient arguments');
-} else {
-  console.log(`${arg1} is ${arg2}`);
-}
+console.log(`${arg1} is ${arg2}`);
