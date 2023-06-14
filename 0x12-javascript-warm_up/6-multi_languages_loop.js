fd1f2585671afa5c71c325c7c626cd4408a98745
emarex1 <emmanuelawoh0@gmail.com>
2023-06-14 10:51:14 +0100

    multi loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let counter = 0;
+while (counter < array.length) {
+  console.log(array[counter]);
+  counter++;
+}
