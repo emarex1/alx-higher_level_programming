c7a9acbd6517cad42f4266450d9f2062ca52a3a7
emarex1 <emmanuelawoh0@gmail.com>
2023-07-06 17:48:56 +0100

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
+"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    value = {'email': argv[2]}
+    res = requests.post(argv[1], data=value)
+    print(res.text)
