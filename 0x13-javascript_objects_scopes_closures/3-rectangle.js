65673446bc76b04833369542871d5be6768eba7a
emarex1 <emmanuelawoh0@gmail.com>
2023-08-04 21:41:25 +0100

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== "number" || typeof h !== "number") {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
       // If w or h is equal to 0 or not a positive integer, create an empty object
       return {};
     }
@@ -10,13 +10,13 @@ module.exports = class Rectangle {
     this.height = h;
   }
 
-  print() {
+  print () {
     if (this.width && this.height) {
       // If both width and height are valid, print the rectangle
       for (let i = 0; i < this.height; i++) {
-        let row = "";
+        let row = '';
         for (let j = 0; j < this.width; j++) {
-          row += "X";
+          row += 'X';
         }
         console.log(row);
       }
